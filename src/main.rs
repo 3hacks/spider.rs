3e501fe0c2eb5c20b575988dd2a7ad76aee22203
lucky <takuto.yoshikai@gmail.com>
2021-07-03 22:27:17 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,27 @@
+use std::env;
+use std::io::Write;
+use std::io;
+use aes::Aes256;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
+use rand::seq::SliceRandom;
+
+type AesCbc = Cbc<Aes256, Pkcs7>;
 use ipfs_api::IpfsClient;
 use ipfs_api::TryFromUri;
 use http::Uri;
-use rsa::{PublicKey, RSAPublicKey, PaddingScheme};
 use std::io::Cursor;
 use base64;
-use rand::rngs::OsRng;
-use ureq;
-use iota_client::{Client, Result};
+use iota_client::{Client};
+const BASE_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const PASSWORD: &str = "SxRzGTuW5PbDLOoSv2up+whMRLBLqSz8";
 
-const PUBKEY: &str = r#"-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArIEzTAaYFFzK75knVvi+
-+RqpbDx/iOGtgH6Ott6Bowx9wxN0IerGFbNei/o6a8vrd0RuwcgtYhqstaYq6jyc
-fJ+AxOzeO0dikhPJSE+86NtYAfHCm5VgHK8Bq4CtHLQMet9TrDgNkMVs3pi1PiET
-OpaCp+Cr0WgR4qg2jeD1/DOpLsxjvQ+C2U1myHvVzyC/zB3QaJglLChf0J2oSE6p
-3XzEjCXL4QESom2qbJqueyVLWByJIIgH5Vyk0LI9iMuuAUbtVUtBAK1NYUafSRKl
-d3SH/ygnqBKByID435qVYi9G7HZHHQ4bQtSaluLXnOc6UeASoPgYH7+hz4VKswXK
-fwIDAQAB
------END PUBLIC KEY-----"#;
-
-fn encrypt(filename: &str) -> Vec<u8> {
-    let pubkey = create_pubkey();
-    let mut rng = OsRng;
-    //let mut file = File::open(filename).expect("Failed to open the file");
-    //let mut buf = Vec::new();
-    //let _ = file.read_to_end(&mut buf).expect("Failed to load the file");
+fn encrypt(filename: &str) -> String {
     let buf = std::fs::read(filename).expect("Failed to load the file");
-    let enc_data = pubkey.encrypt(&mut rng, PaddingScheme::PKCS1v15Encrypt, &buf).expect("Failed to encrypt the data");
+    let enc_data = encrypt_data(PASSWORD, &buf);
     return enc_data;
 }
-fn create_pubkey() -> RSAPublicKey {
-    let der_encoded = PUBKEY
-        .lines()
-        .filter(|line| !line.starts_with("-"))
-        .fold(String::new(), |mut data, line| {
-            data.push_str(&line);
-            data
-        });
-    let der_bytes = base64::decode(&der_encoded).expect("failed to decode base64 content");
-    let public_key = RSAPublicKey::from_pkcs8(&der_bytes).expect("failed to parse key");
-    return public_key;
-}
+
 
 async fn send_to_ipfs(filename: &str) -> String {   
     let uri = "https://ipfs.infura.io:5001/api/v0".parse::<Uri>().unwrap();
@@ -49,21 +31,61 @@ async fn send_to_ipfs(filename: &str) -> String {
 }
 
 async fn send_ipfs_hash(ipfs_hash: &str) {
+    println!("start IOTA");
     let iota = Client::builder()
         .with_node("https://chrysalis-nodes.iota.org").expect("IOTA node is unavailable.")
         .finish()
         .await.expect("IOTA node is unavailable.");
-    println!("new");
-    let message = iota
+    let _ = iota
         .message()
         .with_index("3hacks")
         .with_data(ipfs_hash.as_bytes().to_vec())
         .finish()
         .await.expect("Failed to send message to IOTA.");
-    println!("done");
+    println!("IOTA done");
+}
+
+fn gen_ascii_chars(size: usize) -> String {
+    let mut rng = &mut rand::thread_rng();
+    String::from_utf8(
+        BASE_STR.as_bytes()
+            .choose_multiple(&mut rng, size)
+            .cloned()
+            .collect()
+    ).unwrap()
+}
+
+fn encrypt_data(key: &str, data: &[u8]) -> String {
+    let iv_str = gen_ascii_chars(16);
+    let iv = iv_str.as_bytes();
+    let cipher = AesCbc::new_var(key.as_bytes(), iv).unwrap();
+    let ciphertext = cipher.encrypt_vec(data);
+    let mut buffer = bytebuffer::ByteBuffer::from_bytes(iv);
+    buffer.write_bytes(&ciphertext);
+    base64::encode(buffer.to_bytes())
+}
+
+fn decrypt(filename: &str) {
+    let content = std::fs::read_to_string(filename).expect("Failed to load encrypted file.");
+    let buf = decrypt_data(PASSWORD, &content);
+    let mut writer = io::BufWriter::new(io::stdout());
+    writer.write(&buf).unwrap();
+}
+fn decrypt_data(key: &str, data: &str) -> Vec<u8> {
+    let bytes = base64::decode(data).unwrap();
+    let cipher = AesCbc::new_var(key.as_bytes(), &bytes[0..16]).unwrap();
+    cipher.decrypt_vec(&bytes[16..]).unwrap()
 }
 #[tokio::main]
 async fn main() {
-    let ipfs_hash = send_to_ipfs("hello.txt").await;
-    send_ipfs_hash(&ipfs_hash).await;
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    let target = &args[2];
+    if command == "send" {
+        let ipfs_hash = send_to_ipfs(&target).await;
+        send_ipfs_hash(&ipfs_hash).await;
+    }
+    if command == "receive" {
+        decrypt(&target);
+    }
 }
