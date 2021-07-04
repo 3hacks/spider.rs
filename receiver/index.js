5e41b268bcc7c9fa6664eb91d42d21f05cc6b2f1
lucky <takuto.yoshikai@gmail.com>
2021-07-04 22:17:28 +0900

    rm line

--- receiver/index.js
+++ receiver/index.js
@@ -8,39 +8,31 @@ async function run() {
   const client = new ClientBuilder()
     .node('https://chrysalis-nodes.iota.org')
     .build();
-
   client.subscriber().topics(['messages']).subscribe((err, data) => {
     let payload;
-
     try {
       payload = JSON.parse(data.payload).payload;
     } catch(err) {
       return;
     }
-
     if (payload.type !== "Indexation") {
       return;
     }
-
     const index = payload.data.index.map((n) => {
       return String.fromCharCode(n);
     }).reduce((a, b) => {
       return a + b;
     });
-
     if (index === INDEX) {
       console.log(index);
-
       const data = payload.data.data.map((n) => {
         return String.fromCharCode(n);
       }).reduce((a, b) => {
         return a + b;
       });
-
       console.log(data);
     }
   })
-
   console.log("start");
 }
 
